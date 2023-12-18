437912122fd3b316823c582b9f50cfa2cfe9008e
mashiranna <55777589+mashiranna@users.noreply.github.com>
2023-12-18 19:40:25 +0200

    temporary commit

--- contextual_answers.py
+++ contextual_answers.py
@@ -1,73 +1,87 @@
-from langchain.llms import HuggingFaceHub
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.llms import HuggingFaceHub
+from huggingface_hub import InferenceClient
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings, OpenAIEmbeddings
+# from langchain.embeddings HuggingFaceInferenceAPIEmbeddings
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
-from langchain.vectorstores import FAISS
+from langchain.vectorstores import faiss
 from langchain.chains import RetrievalQA
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from dotenv import load_dotenv
+from langchain.llms import openai
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langchain import hub
+from langchain.schema import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain.chains.question_answering import load_qa_chain
 import os
 
-os.environ['TRANSFORMERS_CACHE'] = './models/'
-# Load environment variables - HUGGINGFACEHUB_API_TOKEN for HuggingFaceEmbeddings
+# Load environment variables - HUGGINGFACEHUB_API_TOKEN, 
 load_dotenv()
-from huggingface_hub import login
 
 api_key = os.getenv('HUGGINGFACEHUB_API_TOKEN')
-login(api_key)
+openai_key = os.getenv("OPENAI_API_KEY")
 
 # Define the ContextualAnswers class
 class ContextualAnswers():
     # Initialize the ContextualAnswers object with a repository ID
-    def __init__(self, repo_id, ) -> None:
-        self.repo_id = repo_id
+    def __init__(self) -> None:
+    # def __init__(self, repo_id) -> None:
+        # self.repo_id = repo_id
         self.llm = None
-        self.embeddings = HuggingFaceEmbeddings()
-        # self.embeddings = HuggingFaceInferenceAPIEmbeddings(api_key=api_key, 
-        #                                                     model_name="intfloat/multilingual-e5-small")
+        #self.embeddings = HuggingFaceEmbeddings()
+        self.embeddings = OpenAIEmbeddings()
         self.qa = None
         self.texts = None
         self.retriever = None
     
     # Load and split documents into chunks    
     def load_and_split_documents(self, uploaded_file):
-        documents = [uploaded_file.read().decode()]
+        # documents = [uploaded_file.read().decode()]
         # Split documents into chunks
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-        self.texts = text_splitter.create_documents(documents)
+        text_splitter = RecursiveCharacterTextSplitter(separators=['\n', ' ', '\n\n'], chunk_size=500, chunk_overlap=100)
+        self.texts = text_splitter.create_documents([uploaded_file])
     
     # Initialize the retriever
     def init_retriever(self):
         # Create a vectorstore from documents
-        db = FAISS.from_documents(documents=self.texts, embedding=self.embeddings)
+        print(len(self.texts))
+        db = faiss.FAISS.from_documents(documents=self.texts, embedding=self.embeddings)
         # Create retriever interface
         self.retriever = db.as_retriever()
     
     # Initialize the language model
     def init_llm(self, local=False, **kwargs):
-        if local:
-            tokenizer = AutoTokenizer.from_pretrained(self.repo_id, token=api_key)
-            self.llm = AutoModelForCausalLM.from_pretrained(self.repo_id, token=api_key)
-            pipe = pipeline("text-generation",
-                               model=self.llm,
-                                tokenizer=tokenizer,
-                     )
-            hf = HuggingFacePipeline(pipeline=pipe)
-            self.llm = hf
+        # if local:
+        #     tokenizer = AutoTokenizer.from_pretrained(self.repo_id, token=api_key)
+        #     self.llm = AutoModelForCausalLM.from_pretrained(self.repo_id, token=api_key)
+        #     pipe = pipeline("text-generation",
+        #                        model=self.llm,
+        #                         tokenizer=tokenizer,
+        #              )
+        #     hf = HuggingFacePipeline(pipeline=pipe)
+        #     self.llm = hf
+        # else:
+            # self.llm = HuggingFaceHub(repo_id=self.repo_id, model_kwargs=kwargs)
+            # self.llm = InferenceClient(model=self.repo_id)
 
-        else:
-            self.llm = HuggingFaceHub(repo_id=self.repo_id, model_kwargs=kwargs)
-          
+        self.llm = openai.OpenAI(temperature=0, api_key=openai_key)
+     
     # Initialize the chain      
     def init_chain(self, chain_type, uploaded_file=None):
-        self.init_llm(local=True)
-        if uploaded_file:
+        self.init_llm()
+        if uploaded_file: 
             self.load_and_split_documents(uploaded_file)
             self.init_retriever()
             self.qa = RetrievalQA.from_chain_type(llm=self.llm, chain_type=chain_type, retriever=self.retriever)
-    
+            
     # Run the chain to get the answer    
     def get_answer(self, query_text):
         if self.qa:
-            return self.qa.run(query_text) 
-          
+            print(self.retriever.get_relevant_documents(query_text))
+            return self.qa({"query": query_text})
+
+        
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from contextual_answers import ContextualAnswers
 from dotenv import load_dotenv
 
-# Load environment variables - you need HUGGINGFACEHUB_API_TOKEN for HuggingFaceEmbeddings
+# Load environment variables - you need HUGGINGFACEHUB_API_TOKEN and OPENAI_API_KEY
 load_dotenv()
 
 # Set the page title in Streamlit
@@ -11,27 +11,38 @@ st.set_page_config(page_title='Contextual Answers')
 st.title('Contextual Answers App')
 
 # Upload a text file to the Streamlit app
-uploaded_file = st.file_uploader('Upload an article', type=['txt'])
+uploaded_file = st.file_uploader('Upload your files', type=['txt'], accept_multiple_files=True)
+
+combined_content = ""
+for file in uploaded_file:
+    file_content = file.getvalue().decode()
+    combined_content += file_content
+
 
 # Input a question to the Streamlit app. The input field is disabled until a file is uploaded.
 query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
 # Initialize the ContextualAnswers object with the specified repository ID
-res = ContextualAnswers(repo_id="meta-llama/Llama-2-7b-hf")
+# res = ContextualAnswers(repo_id="meta-llama/Llama-2-7b-hf") # not enough memory for this one
+# res = ContextualAnswers(repo_id="tiiuae/falcon-7b") # gives uncertain answers
+# res = ContextualAnswers(repo_id="bigscience/bloom-560m") # gives too short answers
+# res = ContextualAnswers(repo_id="tiiuae/falcon-40b") # api call - time out
+# res = ContextualAnswers(repo_id='tiiuae/falcon-7b-instruct') # false answers
+# res = ContextualAnswers(repo_id='google/flan-t5-xxl')
+res = ContextualAnswers()
 
 # Initialize the chain with the specified type and uploaded file
-res.init_chain(chain_type='stuff', uploaded_file=uploaded_file)
+res.init_chain(chain_type='stuff', uploaded_file=combined_content)
 
-# Initialize an empty list to store the results
-result = []
+# Initialize an empty result
+response = None
 
 # Calculate the answer to the query
 with st.spinner('Calculating...'):
-    if res:
+    if res and query_text != '':
         # Get the answer to the query
         response = res.get_answer(query_text)
-        result.append(response)
 
 # If there is a result, display it in an info box
-if result is not None:
-    st.info(response)
+if response is not None:
+    st.info(response.get('result'))
